e3dba25a129bf34f8ea3ef635bfc74b57574a532
David <davidnapadensky@gmail.com>
2020-02-13 02:51:24 -0300

    Updates

---
+++ precache-manifest.7d0de1007f7ca4cd8b1078f3057dfddd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5e3dcba1667af98be7fc69bfbe64fdc",
+    "url": "/Napadensky/reactTask.git/index.html"
+  },
+  {
+    "revision": "78fcd247aed9053f7233",
+    "url": "/Napadensky/reactTask.git/static/css/main.69dec69d.chunk.css"
+  },
+  {
+    "revision": "6061bbbf79c23065cff0",
+    "url": "/Napadensky/reactTask.git/static/js/2.b1a7eed1.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/Napadensky/reactTask.git/static/js/2.b1a7eed1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78fcd247aed9053f7233",
+    "url": "/Napadensky/reactTask.git/static/js/main.73dd6665.chunk.js"
+  },
+  {
+    "revision": "8151db8db5f1ffd88ade",
+    "url": "/Napadensky/reactTask.git/static/js/runtime-main.f8122b66.js"
+  }
+]);
